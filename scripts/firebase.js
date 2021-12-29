0f6a44eb08c97b9496cd55b6c47432c9e63e61c3
CAS <78571139+cas-14@users.noreply.github.com>
2021-12-29 13:05:49 -0500

    it needs work

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -18,18 +18,32 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
+import { getDatabase, ref, set, get, child, onValue } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-database.js";
 
 const database = getDatabase(app);
 
-function writeUserData(userId, name, email) {
+function initNewUser(userId, name) {
     const db = getDatabase();
     set(ref(db, 'users/' + userId), {
-        username: name,
-        email: email
+        groups: null,
+        userinfo: {
+            bio: "",
+            name: name
+        }
     });
 }
 
+function getUserData(userId) {
+    const starCountRef = ref(database, 'users/' + userId + '/userinfo');
+    onValue(starCountRef, (snapshot) => {
+        const data = snapshot.val();
+        updateStarCount(postElement, data);
+    });
+}
+
+
+
+/*
 const dbRef = ref(getDatabase());
 get(child(dbRef, `users/${userId}`)).then((snapshot) => {
     if (snapshot.exists()) {
@@ -40,3 +54,60 @@ get(child(dbRef, `users/${userId}`)).then((snapshot) => {
 }).catch((error) => {
     console.error(error);
 });
+*/
+
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+
+function newUser(email, password, username, name) {
+    createUserWithEmailAndPassword(getAuth(), email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+    
+    initNewUser(username, name)
+}
+
+function signIn(email, password) {
+    signInWithEmailAndPassword(getAuth(), email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
+function signInG() {
+    signInWithPopup(getAuth(), provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+
+    
+}
+
+
